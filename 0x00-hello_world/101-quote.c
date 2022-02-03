ff583f6c0ef28de4672a605efecef4c890592ce7
rafyc <rapha.chemouni@gmail.com>
2022-02-03 17:33:36 +0100

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - the enter point function
+* Return: 0
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
