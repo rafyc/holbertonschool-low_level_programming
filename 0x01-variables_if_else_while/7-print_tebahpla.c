0391684449607bc1f805f5a40b0e95b65f08a257
rafyc <rapha.chemouni@gmail.com>
2022-02-04 17:32:39 +0100

    zywx

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - all
+* Return: 0
+*/
+
+int main(void)
+{
+	int a = 122;
+	/* while loop execution */
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
