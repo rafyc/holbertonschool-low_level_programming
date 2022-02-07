8cabc94d6715ff5b7059030fcda3503ec24ffba2
rafyc <rapha.chemouni@gmail.com>
2022-02-07 18:48:06 +0100

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - main
+* Return: 0
+*/ 
+int main(void)
+	{
+		int a = 48;
+		int b = 49;
+		int c = 50;
+		while (a < 56)
+		{       
+			putchar(a);
+			putchar(b);
+			putchar(c);
+                         
+			if (a < 55)
+			{       
+				putchar(',');
+				putchar(' ');
+			}
+			if (b == 56)
+			{       
+				a++;
+				b = a + 1;
+				c = b + 1;
+			}
+			else if (c == 57)    
+			{       
+				b++;
+				c = b + 1;
+			}
+			else
+			{
+				c++;
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
