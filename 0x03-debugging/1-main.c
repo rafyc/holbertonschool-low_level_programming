3c8542e6a49dd2100c3ecc2b8266288b327c163c
rafyc <rapha.chemouni@gmail.com>
2022-02-09 12:54:23 +0100

    debug

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i); /* i is not incremented, we need i++ */
+       }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+### Debuging
+## For this project, students are expected to look at these concepts:
+
+The Framework
+Concept Mathilde
