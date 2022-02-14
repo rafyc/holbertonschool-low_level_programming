fc0c58ed7472b91a21a6555aadb34eaf983b3bb0
rafyc <rapha.chemouni@gmail.com>
2022-02-14 15:10:23 +0100

    Write a function that prints every other character of a string, starting with the first character, followed by a new line.Write a function that reverses a string.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
 
 void puts2(char *str)
 {
-	long i = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
