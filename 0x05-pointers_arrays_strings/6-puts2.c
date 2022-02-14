4df7eb22069506595728f0245f42411aca319003
rafyc <rapha.chemouni@gmail.com>
2022-02-14 14:55:00 +0100

    Write a function that prints every other character of a string, starting with the first character, followed by a new line.Write a function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2- main function
+ * @str: main param
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
