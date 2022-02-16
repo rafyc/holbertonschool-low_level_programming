8d631914c1c2d1dfd444a959199afb62a29f0cdd
rafyc <rapha.chemouni@gmail.com>
2022-02-16 13:48:46 +0100

    Write a function that copies a string.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - main proto
+ * @dest:
+ * 
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while(dest[i] != '\0')
+	{
+		i++;
+	}
+	while(j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		if(src[j] == '\0')
+		break;
+	}
+	return (dest);
+}
