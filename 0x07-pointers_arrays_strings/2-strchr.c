7d668870048b77f698ce51ca4ce90f253a0be48f
rafyc <rapha.chemouni@gmail.com>
2022-02-22 16:16:43 +0100

    test2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,21 @@
 #include "main.h"
-
 /**
  * _strchr - main function
  * @s: param1
  * @c: param2
  * Return: c, s, null
  */
-
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
-	for (i = 0; s[i] != c; i++);
-	return (s + i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+	}
+	if (s[i] == c)
+		return (s + i);
 	return (0X0);
 }
