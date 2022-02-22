3180b0f5d180e651cbfcd4d921a3aae03356b0da
rafyc <rapha.chemouni@gmail.com>
2022-02-22 16:11:59 +0100

    test2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,7 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			break;
-		}
-	}
-	if (s[i] == c)
-		return (s + i);
+	for (i = 0; s[i] != c; i++);
+	return (s + i);
 	return (0X0);
 }
