d271c6d451df78e9bf347f828fe1be9ebd6f8375
rafyc <rapha.chemouni@gmail.com>
2022-02-24 12:31:40 +0100

    correction

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,6 +12,10 @@ char *_strstr(char *haystack, char *needle)
 	int i = 0;
 	int j = 0;
 
+	if (haystack == NULL)
+	{
+		return (0);
+	}
 	while (haystack[i] != '\0')
 	{
 		j = 0;
@@ -21,10 +25,6 @@ char *_strstr(char *haystack, char *needle)
 			{
 				j++;
 			}
-		}
-		else if (haystack[i] == NULL)
-		{
-			return(0);
 		}
 		else
 		{
