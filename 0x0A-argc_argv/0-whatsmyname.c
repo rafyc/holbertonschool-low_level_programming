2d3afb99332237e694a8243cb866d1bb477ec9a9
rafyc <rapha.chemouni@gmail.com>
2022-02-25 12:29:09 +0100

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,13 +8,10 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	(void)argc;
+	printf("%s\n", *argv);
+		
 	return (0);
 }
