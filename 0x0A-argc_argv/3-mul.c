432278031a4402a96bbf4a07dfe0e31a8e5f744f
rafyc <rapha.chemouni@gmail.com>
2022-02-25 14:20:23 +0100

    multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,16 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	for (i = 1; i < argc; i++)
-	{
-		mul *= atoi(argv[i]);
-	}
-
-	printf("%d\n", mul);
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+		printf("%d\n", mul);
+
 	return (0);
 }
