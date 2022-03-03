e8db5fe6cb56c2e4746d1965117209a70d0bf91f
rafyc <rapha.chemouni@gmail.com>
2022-03-03 10:10:14 +0100

    Write a function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code
+ * @b: param 1
+ * Return: 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == 0)
+		exit (98);
+	return (p);
+}
