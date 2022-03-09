56ccbb4798def538f024b958b2d78c6249ed5a02
rafyc <rapha.chemouni@gmail.com>
2022-03-09 12:24:30 +0100

    1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - 
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size != 0 && cmp =! NULL)
+	{
+		if (size <= 0)
+			return(-1);
+
+		for (i = 0; i < size ; i++)
+		{
+			cmp(array[i]);
+			if (cmp != NULL)
+			{
+				break;
+			}
+		}
+		return(i);
+	}
+}
