ae949704b7068398b02bc094cb0da009fadec6fe
rafyc <rapha.chemouni@gmail.com>
2022-03-09 12:18:01 +0100

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef _function_
+#define _function_
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -7,3 +10,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int _putchar(char c);
+
+#endif
