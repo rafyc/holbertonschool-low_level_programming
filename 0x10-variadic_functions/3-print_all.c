8c8c32ae58b9edd3566484c04e9103bb0e799db1
rafyc <rapha.chemouni@gmail.com>
2022-03-10 12:41:58 +0100

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - fonction
+ * @separator: param
+ * @n: param
+ * Return: rien
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * format_char - char
+ * @a: param1
+ * Return: nothing
+*/
+
+void format_char(char *a)
+{
+	if (a == NULL)
+		printf("(nil)");
+	else
+	printf("%s", *a)
+}
+
