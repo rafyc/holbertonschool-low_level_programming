c8105c84a1805bfa58a836c88a150075df8d3956
rafyc <rapha.chemouni@gmail.com>
2022-03-17 07:22:35 +0100

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * add_node - main function
+ * list_t: head
+ * @str: param
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	
+
+}
