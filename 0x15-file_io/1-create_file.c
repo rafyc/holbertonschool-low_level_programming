cedce206bcedc580d685ac6271d9ade8c6e1a4c5
rafyc <rapha.chemouni@gmail.com>
2022-03-28 15:58:51 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_TRUNC | O_CREAT, 0600);
+	op = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0600);
 
 	if (op == -1)
 		return (-1);
