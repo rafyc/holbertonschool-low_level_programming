8ea425726e303ade44de027f261c7d78ac6afaac
rafyc <rapha.chemouni@gmail.com>
2022-03-28 16:16:55 +0200

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_APPEND);
+	op = open(filename, O_APPEND | O_RDWR);
 
 	if (op == -1)
 		return (-1);
