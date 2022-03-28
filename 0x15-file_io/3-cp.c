b58f8d49df86c3a8d418a43c3d5c62dfc2c782ec
rafyc <rapha.chemouni@gmail.com>
2022-03-28 17:44:32 +0200

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+* main - fonction
+* @argc: para
+* @argv: para
+* Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int cpfrom, closecpfrom, cpto, closecpto, n;
+	char *buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit (97);
+	}
+
+	cpfrom = open(argv[1], O_RDONLY);
+	if (cpfrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit (98);
+	}
+
+	cpto = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	if (cpto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	n = read(cpfrom, buff, 1024);
+
+	while (n)
+	{
+		write (cpto, buff, n);
+	}
+
+	closecpfrom = close(cpfrom);
+	if (closecpfrom == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", cpfrom);
+		exit (100);
+	}
+
+	closecpto = close(cpto);
+	if (closecpto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", cpto);
+		exit (100);
+	}
+	return (0);
+}
