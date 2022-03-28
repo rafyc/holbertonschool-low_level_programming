73c66082cbf4bf05cbd5748adf19fa0916e84eec
rafyc <rapha.chemouni@gmail.com>
2022-03-28 16:15:34 +0200

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - main function
+ * append_text_to_file - main function
  * @filename: param1
  * @text_content: param2
  * Return: int
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_TRUNC | O_APPEND);
+	op = open(filename, O_APPEND);
 
 	if (op == -1)
 		return (-1);
