668108c81feb01a29bc365bd54cbf22aaebe8dfa
rafyc <rapha.chemouni@gmail.com>
2022-03-28 15:28:39 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * create_file - main function
@@ -11,6 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op;
+	int len = 0;
 	int i = 0;
 
 	if (filename == NULL)
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (op == -1)
 		return (-1);
 
-	while (text_content)
-		i++;
+	for (i = 0; text_content[i] != '\0', i++)
+		len++;
 
 	if (text_content != NULL)
-		write(op, text_content, strlen(text_content));
+		write(op, text_content, len);
 
 	close(op);
 	return (1);
