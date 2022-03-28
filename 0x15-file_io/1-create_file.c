4cc32b764805aa4f4f34cf814e773098ab4cc61c
rafyc <rapha.chemouni@gmail.com>
2022-03-28 15:38:12 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	op = open(filename, O_CREAT, O_RDWR, O_TRUNC);
 
 	if (op == -1)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0', i++)
+	while (text_content[i])
+	{
+		i++;
 		len++;
+	}
 
 	if (text_content != NULL)
 		write(op, text_content, len);
