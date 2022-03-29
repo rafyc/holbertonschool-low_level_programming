43e22d5b434c27a9b727767a59b0cade48450eee
rafyc <rapha.chemouni@gmail.com>
2022-03-29 10:47:58 +0200

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,24 +12,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rd, wr, op;
 	char *temp;
 
-	temp = malloc(sizeof(char) * letters);
-	if (temp == NULL)
+	if (filename == NULL)
 		return (0);
 
-	if (filename == NULL)
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
 		return (0);
 
 	op = open(filename, O_RDWR);
 
 	if (op == -1)
+	{
+		free(temp);
 		return (0);
+	}
 
 	rd = read(op, temp, letters);
 	if (rd == -1)
+	{
+		free(temp);
 		return (0);
+	}
 
 	wr = write(STDOUT_FILENO, temp, rd);
 	if (wr == -1)
+	{
+		free(temp);
+		return (0);
+	}
+
+	if (close(op) == -1)
 		return (0);
 
 	close(op);
