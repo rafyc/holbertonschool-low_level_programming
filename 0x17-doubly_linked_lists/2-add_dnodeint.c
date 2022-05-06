21adaebfd253ec34c0c2e4c0e0fd0f774fe07483
Raphaël Chemouni <45367323+rafyc@users.noreply.github.com>
2022-05-06 10:53:24 +0200

    update condition

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,16 +20,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		new_node->next = NULL;
 		*head = new_node;
-		return (new_node);
 	}
 
-	if (*head)
+	else
 	{
 		new_node->next = *head;
 		(*head)->prev = new_node;
 		*head = new_node;
-		return (new_node);
 	}
 
-	return (NULL);
+	return (new_node);
 }
