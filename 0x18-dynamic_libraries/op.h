1a6b453a7f88d4b3a7d51481bcb3015974fbf8cd
rafyc <rapha.chemouni@gmail.com>
2022-05-13 04:40:44 -0400

    operations

---
+++ 0x18-dynamic_libraries/op.h
@@ -0,0 +1,12 @@
+#ifndef __OP__
+#define __OP__
+#include <stdio.h>
+#include <stdlib.h>
+
+int _add(int a, int b);
+int _sub(int a, int b);
+int _div(int a, int b);
+int _mul(int a, int b);
+int _mod(int a, int b);
+
+#endif
