57a02f327b4d63dbe2742a7c62ae799fee31b3a5
rafyc <rapha.chemouni@gmail.com>
2022-05-13 04:55:48 -0400

    op.h

--- 0x18-dynamic_libraries/op.h
+++ 0x18-dynamic_libraries/op.h
@@ -1,12 +1,10 @@
 #ifndef __OP__
 #define __OP__
-#include <stdio.h>
-#include <stdlib.h>
 
-int _add(int a, int b);
-int _sub(int a, int b);
-int _div(int a, int b);
-int _mul(int a, int b);
-int _mod(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
 
 #endif
