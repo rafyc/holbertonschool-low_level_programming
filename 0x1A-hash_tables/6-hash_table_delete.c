7dd0c4ef25006a2bfd3d95b0807c57d3b013b2b3
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-25 17:25:19 +0200

    deletes a hash table.

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - main function that delet elements
+ * @ht: The Hash table you want to delet value / key
+ * Return: nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp, *head;
+
+	if (ht)
+	{
+		for (; i < ht->size; i++)
+		{
+			head = ht->array[i];
+
+			while (head)
+			{
+				tmp = head;
+				head = head->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+			}
+		}
+	}
+}
