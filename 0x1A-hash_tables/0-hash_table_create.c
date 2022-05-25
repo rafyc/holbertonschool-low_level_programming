fe490ee4e38e6ac8cf9f7cbebc525e14d11c1ef2
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-25 15:52:13 +0200

    3- function that adds an element to the hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 
+	new_ht->size = size;
+
 	new_ht->array = malloc(sizeof(hash_node_t) * size);
 	if (new_ht->array == NULL)
 	{
