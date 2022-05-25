9e54f49ed706e82b2c7a6041e34191fc4e63c936
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-05-25 12:28:17 +0200

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+
+	new_ht->array = malloc(sizeof(hash_node_t) * size);
+	if (new_ht->array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+
+	return (new_ht);
+}
